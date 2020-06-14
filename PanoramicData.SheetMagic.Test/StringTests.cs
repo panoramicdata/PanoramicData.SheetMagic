998456a342914a14971f9d9e583246b8724217e7
David Bond <david.bond@panoramicdata.com>
2020-06-14 13:51:36 +0100

    Tweaked string tests

--- PanoramicData.SheetMagic.Test/StringTests.cs
+++ PanoramicData.SheetMagic.Test/StringTests.cs
@@ -9,7 +9,6 @@ namespace PanoramicData.SheetMagic.Test
 		[InlineData("wmi pass", "wmipass")]
 		[InlineData("wmi - (pass)", "wmipass")]
 		[InlineData("1abc", "abc")]
-		[InlineData("2abc", "abc")]
 		[InlineData("abc2", "abc2")]
 		public void TweakStrings(string input, string expectedOutput)
 			=> Assert.Equal(expectedOutput, MagicSpreadsheet.TweakString(input));
